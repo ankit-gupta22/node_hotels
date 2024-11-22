dab53908dd3a3d40520730d2804edd7e62eadd8b
ANIKET-GUPTA31 <aniketgupta2226@gmail.com>
2024-11-22 15:33:48 +0530

    final deployment

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
